ceccd2c62a2a91f1095dc739f24e118e647dac40
Vaibhav Singh <sivaibhav@google.com>
2022-12-22 02:42:36 +0000

    for 4x4x8 slice

--- xla_add/mpu/initialize.py
+++ xla_add/mpu/initialize.py
@@ -15,11 +15,13 @@
 
 
 """Model and data parallel groups."""
-
+import itertools
+import logging
+from typing import Any, Dict, List, Optional, Sequence, Tuple
 import torch
 import torch_xla
 import torch_xla.core.xla_model as xm
-
+import numpy as np
 from .utils import ensure_divisibility
 
 
@@ -207,6 +209,64 @@ def initialize_model_parallel(tensor_model_parallel_size_=1,
             _EMBEDDING_GROUP = group
         if rank in ranks:
             _EMBEDDING_GLOBAL_RANKS = embedding_ranks
+
+    if tensor_model_parallel_size_ == 4:
+        _MODEL_PARALLEL_GLOBAL_GROUP=[[  0,   8,  16,  24],
+       [ 32,  40,  48,  56],
+       [ 64,  72,  80,  88],
+       [ 96, 104, 112, 120],
+       [  1,   9,  17,  25],
+       [ 33,  41,  49,  57],
+       [ 65,  73,  81,  89],
+       [ 97, 105, 113, 121],
+       [  2,  10,  18,  26],
+       [ 34,  42,  50,  58],
+       [ 66,  74,  82,  90],
+       [ 98, 106, 114, 122],
+       [  3,  11,  19,  27],
+       [ 35,  43,  51,  59],
+       [ 67,  75,  83,  91],
+       [ 99, 107, 115, 123],
+       [  4,  12,  20,  28],
+       [ 36,  44,  52,  60],
+       [ 68,  76,  84,  92],
+       [100, 108, 116, 124],
+       [  5,  13,  21,  29],
+       [ 37,  45,  53,  61],
+       [ 69,  77,  85,  93],
+       [101, 109, 117, 125],
+       [  6,  14,  22,  30],
+       [ 38,  46,  54,  62],
+       [ 70,  78,  86,  94],
+       [102, 110, 118, 126],
+       [  7,  15,  23,  31],
+       [ 39,  47,  55,  63],
+       [ 71,  79,  87,  95],
+       [103, 111, 119, 127]]
+        for i in _MODEL_PARALLEL_GLOBAL_GROUP:
+            if rank in i:
+                _MODEL_PARALLEL_GROUP = i
+                break
+        _TENSOR_MODEL_PARALLEL_GROUP = _MODEL_PARALLEL_GROUP
+        _DATA_PARALLEL_GLOBAL_GROUP = [[  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,
+         13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,
+         26,  27,  28,  29,  30,  31],
+       [ 32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,
+         45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,
+         58,  59,  60,  61,  62,  63],
+       [ 64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,
+         77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,
+         90,  91,  92,  93,  94,  95],
+       [ 96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+        109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
+        122, 123, 124, 125, 126, 127]]
+        for i in _DATA_PARALLEL_GLOBAL_GROUP:
+            if rank in i:
+                _DATA_PARALLEL_GROUP = i
+           
+
+
+
 
 
 
@@ -539,4 +599,3 @@ def get_args():
 
     args = dummy_args(torch.float, True, False)
     return args
-
